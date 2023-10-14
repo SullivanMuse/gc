c47b19308158a1734d4e1042c8ff8e5da0244e4c
Sullivan Muse <sullivan.muse@gmail.com>
2023-10-14 12:19:39 -0500

    Working implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,149 +1,157 @@
 use std::{alloc::{alloc, dealloc, handle_alloc_error, Layout}, ptr::null_mut};
 
-/// Implement this on objects containing Gc references
-trait Trace {
-    /// Call mark on each Gc
-    fn trace(&self);
-
-    /// The value that will be initialized in a new GcBox
-    fn uninit() -> Self;
-}
-
-/// Container for T objects
-#[derive(Debug)]
-struct GcBox<T> {
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct GcBox {
     next: *mut Self,
     visited: bool,
-    value: T,
+    value: i64,
+    children: Vec<Gc>,
 }
 
-/// Smart pointers for GcBox
-#[derive(Debug)]
-struct Gc<T>(*mut GcBox<T>);
-
-impl<T> Clone for Gc<T> {
-    fn clone(&self) -> Self {
-        Gc(self.0)
-    }
-}
-
-impl<T> Gc<T> {
-    fn mark(&self) {
-        unsafe { &mut *self.0 }.visited = true;
-    }
-
-    fn mutate<F: FnOnce(&mut T)>(&self, f: F) {
-        f(unsafe { &mut (*self.0).value })
-    }
-}
-
-/// Object responsible for allocating and collecting GcBox objects
-#[derive(Debug)]
-struct GcCollector<T> {
-    next: *mut GcBox<T>,
-}
-
-impl<T: std::fmt::Debug> GcCollector<T> {
-    /// Create a new GcCollector
-    pub fn new() -> Self {
-        Self { next: null_mut() }
-    }
-
-    /// Collect GcBoxes that are not reachable from the stack
-    pub fn collect<U: Trace>(&mut self, stack: &[U]) {
-        for value in stack {
-            value.trace();
+impl GcBox {
+    fn new() -> Self {
+        Self {
+            next: null_mut(),
+            visited: false,
+            value: 0,
+            children: Vec::new(),
         }
-        unsafe {
-            let Some(mut prev) = self.next.as_mut() else { return; };
-            if !prev.visited {
-                dbg!(&prev);
-                dealloc((prev as *mut GcBox<T>) as *mut u8, Layout::new::<GcBox<T>>());
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Gc(*mut GcBox);
+
+impl Gc {
+    fn get(&self) -> &GcBox {
+        unsafe { & *self.0 }
+    }
+
+    unsafe fn get_mut(&self) -> &mut GcBox {
+        unsafe { &mut *self.0 }
+    }
+
+    fn set_next(&self, next: *mut GcBox) {
+        unsafe { self.get_mut() }.next = next;
+    }
+
+    fn get_next(&self) -> *mut GcBox {
+        self.get().next
+    }
+
+    fn set_value(&self, value: i64) {
+        unsafe { self.get_mut() }.value = value;
+    }
+
+    fn push(&self, other: &Self) {
+        unsafe { self.get_mut() }.children.push(other.clone());
+    }
+
+    fn mark(&self) {
+        let gcbox = unsafe { self.get_mut() };
+        if !gcbox.visited {
+            gcbox.visited = true;
+            for child in &gcbox.children {
+                child.mark();
             }
-            while let Some(curr) = prev.next.as_mut() {
-                if curr.visited {
-                    prev = curr;
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct GcMac {
+    head: GcBox,
+    stack: Vec<Gc>,
+}
+
+impl GcMac {
+    fn new() -> Self {
+        Self {
+            head: GcBox::new(),
+            stack: Vec::new(),
+        }
+    }
+
+    fn mark(&self) {
+        for value in &self.stack {
+            value.mark();
+        }
+    }
+
+    fn sweep(&mut self) {
+        let layout = Layout::new::<GcBox>();
+        let mut prev = &mut self.head as *mut GcBox;
+        while !prev.is_null() {
+            unsafe {
+                let next = (*prev).next;
+                if (*next).visited {
+                    prev = next;
                 } else {
-                    prev.next = curr.next;
-                    dbg!(&curr);
-                    dealloc((curr as *mut GcBox<T>) as *mut u8, Layout::new::<GcBox<T>>());
+                    // reroute prev to the box after the next and deallocate the box
+                    (*prev).next = (*next).next;
+                    println!("{:?}", (*next).value);
+                    dealloc(next as *mut u8, layout);
                 }
             }
         }
     }
 
-    /// Allocate a GcBox and return a reference object for it
-    pub fn alloc(&mut self) -> Gc<T>
-    where
-        T: Trace,
-    {
-        let layout = Layout::new::<GcBox<T>>();
+    fn collect(&mut self) {
+        self.mark();
+        self.sweep();
+    }
 
-        // Allocate and check allocation
-        let ptr = unsafe { alloc(layout) }.cast::<GcBox<T>>();
-        if ptr.is_null() {
-            handle_alloc_error(layout);
-        }
+    fn alloc(&mut self) -> Gc {
+        let layout = Layout::new::<GcBox>();
+        let r = unsafe {
+            let ptr = alloc(layout);
+            if ptr.is_null() {
+                handle_alloc_error(layout);
+            }
+            let ptr = ptr as *mut GcBox;
+            *ptr = GcBox::new();
+            Gc(ptr)
+        };
+        r.set_next(self.head.next);
+        self.head.next = r.0;
+        r
+    }
 
-        // Construct value
-        unsafe {
-            *ptr = GcBox {
-                next: self.next,
-                visited: false,
-                value: T::uninit(),
-            };
-        }
-        self.next = ptr;
-
-        Gc(self.next)
+    fn push(&mut self, obj: &Gc) {
+        self.stack.push(obj.clone());
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
+fn main() {
+    let mut gc = GcMac::new();
+    let a = gc.alloc();
+    a.set_value(0);
+    let b = gc.alloc();
+    b.set_value(1);
+    let c = gc.alloc();
+    c.set_value(2);
+    let d = gc.alloc();
+    d.set_value(3);
+    let e = gc.alloc();
+    e.set_value(4);
+    let f = gc.alloc();
+    f.set_value(5);
 
-    #[derive(Debug)]
-    struct Graph {
-        value: i64,
-        children: Vec<Gc<Self>>,
+    a.push(&b);
+    b.push(&c);
+    c.push(&a);
+
+    d.push(&e);
+    e.push(&f);
+    f.push(&d);
+
+    gc.push(&a);
+    for x in [&a, &b, &c, &d, &e, &f] {
+        dbg!(x.get().value, x.get().visited);
     }
-
-    impl Trace for Graph {
-        fn trace(&self) {
-            for child in &self.children {
-                child.mark();
-            }
-        }
-
-        fn uninit() -> Self {
-            Self { value: 0, children: Vec::new() }
-        }
+    gc.mark();
+    for x in [&a, &b, &c, &d, &e, &f] {
+        dbg!(x.get().value, x.get().visited);
     }
-
-    /// 1 -> 2 -> 3 -> 1
-    /// 4 -> []
-    /// [1]
-    #[test]
-    fn test() {
-        let mut gc: GcCollector<Graph> = GcCollector::new();
-
-        let x1 = gc.alloc();
-        let x2 = gc.alloc();
-        let x3 = gc.alloc();
-        let x4 = gc.alloc();
-        x1.mutate(|x| { x.value = 1; x.children.push(x2.clone()); });
-        x2.mutate(|x| { x.value = 2; x.children.push(x3.clone()); });
-        x3.mutate(|x| { x.value = 3; x.children.push(x1.clone()); });
-        x4.mutate(|x| { x.value = 4; });
-        let g1 = Graph {
-            value: 5,
-            children: vec![x1],
-        };
-        let g2 = Graph {
-            value: 6,
-            children: vec![x4],
-        };
-        gc.collect(&[g1]);
-    }
+    gc.sweep();
 }
